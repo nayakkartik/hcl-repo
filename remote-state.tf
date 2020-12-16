99cc5fe1e430674307437cbb9f16c235140060c6
nayakkartik <nayak.kartik@gmail.com>
2020-12-16 18:08:45 +0530

    Update remote-state.tf

--- remote-state.tf
+++ remote-state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket  = "kartik-hcl-new"
-    key  = "state/state"
+    bucket  = "kartik-new-01"
+    key  = "test01/state"
     region = "us-east-2"
 #   access_key = "XXXXXXXXXXXXXXXXXXXXXX"
 #   secret_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
